ae1fd4e663732f685ce200ec85ec7289342c9b90
victor <poltoratchi1369@gmail.com>
2021-09-14 15:03:42 +0300

    fix name of library in stack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,32 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const HTMLWebpackPlugin = require( 'html-webpack-plugin' );
+const uglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+
+const PATHS = {
+    src: `${__dirname}/src/`,
+    dist: `${__dirname}/dist`
+};
+const libraryName = 'pagination';
 
 module.exports = {
     // mode: 'development',
     mode: 'production',
-    entry: './src/index.js',
+    entry: `${PATHS.src}index.js`,
     output: {
-        library: 'UserList',
+        library: libraryName,
         libraryTarget: 'umd',
         libraryExport: 'default',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        path: path.resolve( __dirname, 'dist' ),
+        filename: `${libraryName}.js`
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: [ 'babel-loader' ]
             },
             {
                 test: /\.scss$/,
@@ -34,9 +41,14 @@ module.exports = {
     },
     plugins: [
         new uglifyJsPlugin(),
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html')
-        }),
+        new HTMLWebpackPlugin( {
+            template: path.resolve( __dirname, 'index.html' )
+        } ),
         new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin( {
+            patterns: [
+                {from: `${PATHS.src}demo.js`, to: `${PATHS.dist}`}
+            ]
+        } )
     ]
 };
